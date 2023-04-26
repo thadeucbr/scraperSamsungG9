cd9a8cecec1cbe5da7ae9060bdef7f93dfdeb388
thadeucbr <thadeucb@hotmail.com>
2023-04-26 07:29:19 -0300

    alterado para acompanhar o preco de 2 produtos

--- monitorar_preco.py
+++ monitorar_preco.py
@@ -9,18 +9,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+import threading
 
 load_dotenv()
 
 EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-TARGET_PRICE = 8600  # Valor máximo que você deseja pagar pelo monitor
 
-def send_email(price):
+print("Iniciando o script monitorar_preco.py")
+
+def send_email(price, product_name, url):
     msg = EmailMessage()
-    msg.set_content(f"O preço do Monitor Gamer Curvo Samsung Odyssey 49 caiu para R${price}!")
+    msg.set_content(f"O preço do {product_name} caiu para R${price}!\n\nConfira a oferta aqui: {url}")
 
-    msg["Subject"] = "Alerta de Preço: Monitor Gamer Curvo Samsung Odyssey 49"
+    msg["Subject"] = f"Alerta de Preço: {product_name}"
     msg["From"] = EMAIL_ADDRESS
     msg["To"] = EMAIL_ADDRESS
 
@@ -54,28 +56,46 @@ def obter_preco(url):
     driver.quit()
     return price
 
+ultimo_preco_por_url = {}
 
-def monitorar_preco():
-    url = 'https://shop.samsung.com.br/monitor-gamer-curvo-samsung-odyssey-49/p?cid=br_site_e-store_neoqledgamer_full__link_monitocurvorodysseygamer49_none'
+def monitorar_preco(url, product_name):
+    global ultimo_preco_por_url
 
     while True:
         preco = obter_preco(url)
 
         if preco:
-            print(f"O preço atual do monitor é: {preco}")
+            print(f"O preço atual do {product_name} é: {preco}")
             
-            # Remova o caractere 'R$' e converta a string para um número float
             preco_num = float(preco.replace("R$", "").replace(".", "").replace(",", "."))
 
+            if url not in ultimo_preco_por_url or preco_num < ultimo_preco_por_url[url]:
+                if url in ultimo_preco_por_url:
+                    print(f"O preço do {product_name} caiu! Enviando e-mail...")
+                    send_email(preco, product_name, url)
 
-            if preco_num <= TARGET_PRICE:
-                print("O preço caiu! Enviando e-mail...")
-                send_email(preco)
+                ultimo_preco_por_url[url] = preco_num
 
         else:
-            print("Não foi possível obter o preço do monitor.")
+            print(f"Não foi possível obter o preço do {product_name}.")
 
-        time.sleep(60 * 60)  # Aguarde uma hora (3600 segundos) antes de verificar novamente
+        time.sleep(60 * 60)
+
+def main():
+    url1 = 'https://shop.samsung.com.br/monitor-gamer-curvo-samsung-odyssey-49/p?cid=br_site_e-store_neoqledgamer_full__link_monitocurvorodysseygamer49_none'
+    product_name1 = 'Monitor Gamer Curvo Samsung Odyssey 49'
+
+    url2 = 'https://shop.samsung.com/br/monitor-gamer-curvo-samsung-odyssey-49--mini-led-dqhd-240hz-2002/p'
+    product_name2 = 'Monitor Curvo Samsung Odyssey Neo G9 49'
+
+    thread1 = threading.Thread(target=monitorar_preco, args=(url1, product_name1))
+    thread2 = threading.Thread(target=monitorar_preco, args=(url2, product_name2))
+
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
 
 if __name__ == '__main__':
-    monitorar_preco()
+    main()
